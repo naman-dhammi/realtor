7f1437c918205f57b0b1b561eeb20a0715f78607
Naman Dhammi <namandhammi@Mac.lan>
2026-05-24 15:02:39 +0530

    enabled remote mongoDB database connection

--- configurations.py
+++ configurations.py
@@ -5,7 +5,10 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 # Mongo DB
-MONGO_DB_CONNECTION: str = os.environ.get("MONGO_DB_CONNECTION")
+MONGO_DB_USERNAME = os.getenv("MONGO_DB_USERNAME")
+MONGO_DB_PASSWORD = os.getenv("MONGO_DB_PASSWORD")
+MONGO_DB_CLUSTER = os.getenv("MONGO_DB_CLUSTER")
+MONGO_DB_CONNECTION: str = rf"mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@dhammi-enterprise-clust.inkw6eu.mongodb.net/?appName={MONGO_DB_CLUSTER}"
 MONGO_DB_DATABASE: str = os.environ.get("MONGO_DB_DATABASE")
 MONGO_DB_REAL_ESTATE_TABLE: str = os.environ.get("MONGO_DB_REAL_ESTATE_TABLE")
 MONGO_DB_USER: str = os.environ.get("MONGO_DB_USERS")
--- mongodb_manager.py
+++ mongodb_manager.py
@@ -1,11 +1,11 @@
-import pymongo
-
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 from configurations import MONGO_DB_CONNECTION, MONGO_DB_DATABASE
 
 
 # Mongo DB Manager
 class MongoDBManager:
-    client = pymongo.MongoClient(MONGO_DB_CONNECTION)
+    client = MongoClient(MONGO_DB_CONNECTION, server_api=ServerApi('1'))
     db = client[MONGO_DB_DATABASE]
 
     def getData(self, table: str, uuid: str):
