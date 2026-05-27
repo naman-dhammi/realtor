a5324b1b203005fb4e5fdafabbf74d331bfe914d
Naman Dhammi <namandhammi@Namans-MacBook-Air.local>
2026-05-27 14:03:01 +0530

    fixed description display for mobile

--- app.py
+++ app.py
@@ -99,9 +99,9 @@ if __name__ == "__main__":
             st.markdown(property_content, unsafe_allow_html=True)
         for_sale = f"""
         <div class=container style='display:flex; width:100%; gap:0px;'>
-            <div class=box style='background: rgb(255,255,255);font-size:18px;box-shadow:none;width:10%;'>🏠</div> 
-            <div class=box style='background: rgb(255,255,255);font-size:12px;box-shadow:none;width:20%;'><b>Description</b></div> 
-            <div class=box style='background: rgb(255,255,255);font-size:12px;box-shadow:none;width:70%;'>{property["description"]}</div>
+            <div style='background: rgb(255,255,255);font-size:18px;box-shadow:none;width:3%;'>🏠</div> 
+            <div class=box style='background: rgb(255,255,255);font-size:10px;box-shadow:none;width:15%;'><b>Description</b></div> 
+            <div class=box style='background: rgb(255,255,255);font-size:12px;box-shadow:none;width:82%;'>{property["description"]}</div>
         </div>
         <div style="width: 100%;background: linear-gradient(135deg, #22c55e, #16a34a);color: white;padding: 2px;border-radius: 14px;text-align: center;box-shadow: 0 4px 12px rgba(34, 197, 94, 0.25);">For Sale</div>
         """
